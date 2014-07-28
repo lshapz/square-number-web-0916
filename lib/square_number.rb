8ac3dc004314c3217c7f92c99cdd32e782fcbb8d
Katie Hoffman <ktahoffman@gmail.com>
2014-07-28 12:15:50 -0400

    add solution

--- lib/square_number.rb
+++ lib/square_number.rb
@@ -1,5 +1,12 @@
 class Integer
   def square_number?
-    # code goes here
+    counter = 0
+    until counter > self
+      if self == counter * counter
+        return counter
+      end
+      counter += 1
+    end
+    false
   end
 end
