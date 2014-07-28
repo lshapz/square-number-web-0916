4267b791add0860ebfa032f7174e9b9ddd3216bf
Katie Hoffman <ktahoffman@gmail.com>
2014-07-28 13:27:01 -0400

    refactor to not multiply after product exceeds self'

--- lib/square_number.rb
+++ lib/square_number.rb
@@ -1,7 +1,7 @@
 class Integer
   def square_number?
     counter = 0
-    until counter > self
+    until counter * counter > self
       if self == counter * counter
         return counter
       end
