69b870924638d5af171396f6451fcb9027e515f1
lshapz <laurashapiro01@gmail.com>
2016-12-19 15:13:49 -0500

    Done.

--- lib/square_number.rb
+++ lib/square_number.rb
@@ -1,5 +1,16 @@
 class Integer
   def square_number?
+    num = self
+    half = num / 2
+
+    roots = (1..half).select do |number| 
+      (number * number) == num 
+    end
+    if roots.empty? 
+      return false
+    else 
+      return roots[0]
+    end
     # code goes here
   end
 end
